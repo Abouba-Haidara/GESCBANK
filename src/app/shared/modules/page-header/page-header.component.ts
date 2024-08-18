2e53debc936478dec41c28a8775f58b0cf4d19da
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2024-08-18 21:49:16 +0000

    UPDATE

--- src/app/shared/modules/page-header/page-header.component.ts
+++ src/app/shared/modules/page-header/page-header.component.ts
@@ -6,12 +6,15 @@ import { Component, Input, OnInit, TemplateRef } from '@angular/core';
     styleUrls: ['./page-header.component.scss']
 })
 export class PageHeaderComponent  {
+    user_role: string =  'admin';
     public navs =  [
-        { path: '/home', name: 'Accueil' },
-        { path: '/groups', name: 'Groupe' },
-        { path: '/employees', name: 'Employé' },
-        { path: '/clients', name: 'Clients' },
-        { path: '/comptes', name: 'Comptes' },
-        { path: '/operations', name: 'Opérations' }
+        { path: '/home', name: 'Accueil', role: 'admin' },
+        { path: '/groups', name: 'Groupes',  role: 'admin' },
+        { path: '/employees', name: 'Employés',  role: 'admin' },
+        { path: '/clients', name: 'Clients',  role: 'admin' },
+        { path: '/comptes', name: 'Comptes',  role: 'admin' },
+        { path: '/operations', name: 'Opérations',  role: 'admin' },
+        { path: '/dashboard', name: 'Dashboard',  role: 'admin' },
+        { path: '/espace-client', name: 'Espace Client',  role: 'client' }
     ]
  }
